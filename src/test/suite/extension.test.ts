0b69767a1208c81807d62d6368bb580110987c63
norbert-boldizsar <norbert.boldizsar@bee-tf.ro>
2021-09-21 08:39:58 +0300

    WIP: tests.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -8,8 +8,22 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	test('Open extension test', async () => {
+		assert.ifError(
+			await vscode.commands.executeCommand('onCommand:visual-studio-code-plugin-for-alm-octane.details')
+		);
 	});
+
+	test('Open search element test', async () => {
+		assert.ifError(
+			await vscode.commands.executeCommand('visual-studio-code-plugin-for-alm-octane.quickPick')
+		);
+	});
+
+	test('Copy commit message test', async () => {
+		assert.ifError(
+			await vscode.commands.executeCommand('visual-studio-code-plugin-for-alm-octane.commitMessage')
+		);
+	});
+
 });
