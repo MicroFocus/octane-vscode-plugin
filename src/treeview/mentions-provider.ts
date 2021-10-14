1d755eb353fdb8930603d67fc8b3a998b7a40235
Béla Csete <bela@bee-tf.ro>
2021-10-14 12:01:53 +0300

    Defect 278012: Fixed.

--- src/treeview/mentions-provider.ts
+++ src/treeview/mentions-provider.ts
@@ -1,5 +1,8 @@
+import * as vscode from 'vscode';
 import { MyWorkProvider, MyWorkItem } from "./my-work-provider";
 import { OctaneEntity } from "../octane/model/octane-entity";
+import { Comment } from '../octane/model/comment';
+import { OctaneEntityEditorProvider } from '../details/octane-editor';
 
 export class MyMentionsProvider extends MyWorkProvider {
 
@@ -8,9 +11,14 @@ export class MyMentionsProvider extends MyWorkProvider {
         return result;
     }
 
-    getMyWorkItem(i: OctaneEntity): MyWorkItem {
+    getMyWorkItem(i: Comment): MyWorkItem {
         const item = super.getMyWorkItem(i);
         item.contextValue = 'comment';
+        if (i.ownerWorkItem) {
+            item.command = { command: 'vscode.openWith', title: 'Details', arguments: [vscode.Uri.parse(`octane:///octane/${i.ownerWorkItem?.type}/${i.ownerWorkItem?.subtype}/${i.ownerWorkItem?.id}`), OctaneEntityEditorProvider.viewType] };
+        } else {
+            item.command = undefined;
+        }
         return item;
     }
 }
