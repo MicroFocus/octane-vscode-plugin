9478186368277721dec79a64f7b88a9c21cfe672
Béla Csete <bela@bee-tf.ro>
2021-11-11 13:30:26 +0200

    Defect 290016: Fixed.

--- src/details/octane-editor.ts
+++ src/details/octane-editor.ts
@@ -286,6 +286,8 @@ function getDataForSubtype(entity: OctaneEntity | undefined): [string, string] {
         if (entity?.subtype === 'gherkin_test') { return ["GT", "#00a989"]; }
         if (entity?.subtype === 'test_suite') { return ["TS", "#271782"]; }
         if (entity?.subtype === 'requirement_document') { return ["RD", "#0b8eac"]; }
+        if (entity?.subtype === 'run_suite') { return ["SR", "#5216ac"]; }
+        if (entity?.subtype === 'run_manual') { return ["MR", "#29ceff"]; }
     }
     return ['', ''];
 }
