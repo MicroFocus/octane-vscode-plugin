60bedadef1ab56c68e038f380c531f0465f89374
norbert-boldizsar <norbert.boldizsar@bee-tf.ro>
2021-12-02 14:53:58 +0200

    Defect-299005: Fixed font and size.

--- src/details/octane-editor.ts
+++ src/details/octane-editor.ts
@@ -459,7 +459,7 @@ export class OctaneEntityEditorProvider implements vscode.CustomReadonlyEditorPr
                         <span class="label">${getDataForSubtype(data)[0]}</span>
                     </div>
                     <div class="name-container">
-                        <h6 style="margin: 0rem 0.5rem 0rem 0rem;">${data?.id ?? ''}</h6> <input style="background: transparent;" id="name" type="text" value="${data?.name ?? ''}">
+                        <h6 style="margin: 0rem 0.5rem 0rem 0rem;">${data?.id ?? ''}</h6> <input style="background: transparent; font-size: 1rem;" id="name" type="text" value="${data?.name ?? ''}">
                     </div>
                     <div class="action-container">
                         ${await generatePhaseSelectElement(data, fields, activeFields)}
