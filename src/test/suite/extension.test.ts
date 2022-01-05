a307a2792a599271956c1ee81999cfcc49072a7e
Béla Csete <bela@bee-tf.ro>
2022-01-05 12:11:34 +0200

    user story #232010: Added automated test support.

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -3,26 +3,15 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
+import { MyWorkItem, MyWorkItemLabel } from '../../treeview/my-work-provider';
 // import * as myExtension from '../../extension';
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Open extension test', async () => {
-		assert.ifError(
-			await vscode.commands.executeCommand('onCommand:visual-studio-code-plugin-for-alm-octane.details')
-		);
-	});
-
 	test('Open search element test', async () => {
 		assert.ifError(
 			await vscode.commands.executeCommand('visual-studio-code-plugin-for-alm-octane.quickPick')
-		);
-	});
-
-	test('Copy commit message test', async () => {
-		assert.ifError(
-			await vscode.commands.executeCommand('visual-studio-code-plugin-for-alm-octane.commitMessage')
 		);
 	});
 
