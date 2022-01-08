2c2b18e07210fcfede16960ee7bff902bf20edbb
Béla Csete <bela@bee-tf.ro>
2022-01-08 23:17:33 +0200

    Added tooltip support to input template.

--- src/details/fields/text-input-template.ts
+++ src/details/fields/text-input-template.ts
@@ -10,8 +10,12 @@ export class TextInputTemplate extends AbstractFieldTemplate {
 
     generateInputField(): string {
         return `
-            <input style="border: 0.5px solid; border-color: var(--vscode-dropdown-border);" id="${this.field.name}" ${this.generateType()} value='${this.value}' ${this.generateReadonly()} ${this.generateAdditionalAttributes()}>
+            <input style="border: 0.5px solid; border-color: var(--vscode-dropdown-border);" id="${this.field.name}" ${this.generateTooltip()} ${this.generateType()} value='${this.value}' ${this.generateReadonly()} ${this.generateAdditionalAttributes()}>
         `;
+    }
+
+    protected generateTooltip(): string {
+        return `title="${this.value}"`;
     }
 
     protected generateType() {
