8cf5a678993c9ffe5d92bab196de3573998a743c
norbert-boldizsar <norbert.boldizsar@bee-tf.ro>
2022-01-27 10:50:18 +0200

    WIP: Adding regular exp. for finding src in image tag.

--- src/details/fields/description-field-input-template.ts
+++ src/details/fields/description-field-input-template.ts
@@ -1,7 +1,10 @@
 import { TextInputTemplate } from "./text-input-template";
 import { stripHtml } from 'string-strip-html';
+import { OctaneService } from "../../octane/service/octane-service";
 
 export class DescriptionFieldInputTemplate extends TextInputTemplate {
+
+    protected service = OctaneService.getInstance();
 
     constructor(field: any, entity: any, visible: boolean) {
         super(field, entity, visible);
@@ -22,6 +25,14 @@ export class DescriptionFieldInputTemplate extends TextInputTemplate {
 
     protected generateValueFromHTML(): string {
         if (this.value) {
+            let matchImage = this.value.match(/<img [^>]*src="([^"]+)"[^>]*>/);
+            if (matchImage && matchImage[1]) {
+                let src = matchImage[1];
+                let idOfAttachment = src.match(/(attachments\/)([0-9]+)\//);
+                if (idOfAttachment && idOfAttachment[2]) {
+                    // let content = await this.service.downloadAttachmentContent(parseInt(idOfAttachment[2]));
+                }
+            }
             return this.value;
         }
         return '';
@@ -32,7 +43,7 @@ export class DescriptionFieldInputTemplate extends TextInputTemplate {
     }
 
     protected generateReadonly(): string {
-       return 'readonly';
+        return 'readonly';
     }
 
 }
